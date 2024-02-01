3db51414771d0b76a87c879a133372fe30fa6839
DHEV MUGUNDDHAN A <104367907+Dhev-Mugunddhan-A@users.noreply.github.com>
2024-02-01 21:55:45 +0530

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //imports the iostream module that provides utility functions for accessing Input and Output streams.
+using namespace std; 
+
+//the driving function or the main function of the program. Execution of your code starts from this function
+int main(){ //all c++ function must have a default return type of int
+
+    cout <<"Hello"<<" "<<"World"<<endl; //Prints "Hello World" in the terminal
+    return 0;
+}
